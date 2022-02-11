5cc4e93de4ed35486104f621e9fe3068dab00092
leonmavr <0xleo.git@gmail.com>
2022-02-11 11:16:47 +0100

    Make script dir a simple variable
    not an array

--- scripts/clang_format.sh
+++ scripts/clang_format.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-declare -a script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+script_dir=`cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd`
 
 declare -a src_files=($script_dir/../main.cpp $script_dir/../include/*.hpp $script_dir/../src/*.cpp)
-
 for s in "${src_files[@]}"; do
     clang-format -i $s
 done
