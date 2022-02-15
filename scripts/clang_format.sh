ae857f50cb5df0aee675322d44664b90c92ccc96
leonmavr <0xleo.git@gmail.com>
2022-02-15 02:35:03 +0100

    Check whether clang-format is installed

--- scripts/clang_format.sh
+++ scripts/clang_format.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-script_dir=`cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd`
+# This script formats the spacings in your code according
+# to clang's standards
 
+which clang-format > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo -e "ERROR: clang-format is not installed\n"\
+        "Please install it to run this script."
+    exit 1
+fi
+
+script_dir=`cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd`
 declare -a src_files=($script_dir/../main.cpp $script_dir/../include/*.hpp $script_dir/../src/*.cpp)
 for s in "${src_files[@]}"; do
     clang-format -i $s
 done
-
